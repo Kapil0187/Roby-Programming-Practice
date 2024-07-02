9c4ac0bebbae7b2380f6f74623fbec855973608c
Kapil <kapilbarsker2003@gmail.com>
2024-07-02 17:43:59 +0530

    Examples of mudules and mixings

--- exm_modules.rb
+++ exm_modules.rb
@@ -1,3 +1,4 @@
+require_relative "./module1.rb"
 module M1
   def sum(num1, num2)
     val = num1 + num2
@@ -16,8 +17,10 @@ end
 class M3
   include M1
   include M2
+  include M4
 end
 
 obj1 = M3.new()
 obj1.sum(3,4)
 obj1.subtraction(6,4)
+obj1.multiplication(7,8)
---
+++ module1.rb
@@ -0,0 +1,6 @@
+module M4
+  def multiplication(num1,num2)
+    val = num1 * num2
+    puts "Multiplication two number is : "+val.to_s;
+  end
+end
