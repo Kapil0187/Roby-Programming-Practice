9ac81dc8c6c2e21d9831cb4f1f1307ea2c0a24b5
Kapil <kapilbarsker2003@gmail.com>
2024-07-02 14:45:03 +0530

    exmaples of string

---
+++ exm_string.rb
@@ -0,0 +1,41 @@
+# puts 'i am kapil'
+# puts 'i am from india'
+
+# msg = 'This is the example of strings'
+
+# p msg["example"]
+# p msg["of"]
+# puts msg[0,2]
+# puts msg[-3]
+# puts msg[0,msg.length]
+# puts msg[0,12]
+
+# puts "
+# A
+# AB
+# ABC
+# ABCD
+# "
+# puts %/
+# A
+# AB
+# ABC
+# ABCD/
+
+# puts STRING
+# A
+# AB
+# ABC
+# ABCD
+# STRING
+
+# String Concate
+
+# string = "Hi"+" "+"I"+" "+"am"+" "+"Kapil"
+# puts(string);
+
+# string = "Hii"+" "<<"I"+" "<<"am"+" "<<"Kapil"
+# puts(string)
+
+string = "Hii".concat(" ")<<"I"+" ".concat("am")+" "<<"Kapil"
+puts(string)
